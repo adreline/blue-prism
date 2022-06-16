93a38ed680dfcac5598eb20c6d4cc1b970296916
Ubuntu <maddie.madlad@gmail.com>
2022-06-16 10:14:55 +0200

    simplified constructor, added css file route

--- frontend/server.js
+++ frontend/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 exports.Frontend = class{
-    constructor(db_handle,views){
+    constructor(db_handle,root){
         this.db = db_handle;
         this.app = express()
-        this.app.set('views', views)
+        this.app.set('views', `${root}/frontend/views`)
         this.app.set('view engine', 'pug')
         this.app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -16,6 +16,9 @@ exports.Frontend = class{
                 res.render('index',{links: data})
             })
             
+        })
+        this.app.get("/app.css", (req, res)=>{
+            res.sendFile(`${root}/frontend/views/${req.url}`)
         })
     }
     serve(){

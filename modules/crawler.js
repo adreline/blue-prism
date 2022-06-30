308535f5b041426671eafda4f5d2b84a6518faa2
Ubuntu <maddie.madlad@gmail.com>
2022-06-30 11:38:07 +0200

    now possible to change default agent header

--- modules/crawler.js
+++ modules/crawler.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const ProxyAgent = require('simple-proxy-agent');
-const { onion } = require('../config.json');
+const { onion, settings } = require('../config.json');
 
 
 
@@ -10,7 +10,8 @@ async function pullWebsite(link){
             agent: new ProxyAgent(`socks5://${onion.host}:${onion.port}`, {
                 tunnel: true,
                 timeout: onion.connectionTimeout,
-            })
+            }),
+            headers: {'User-Agent': settings.identity}
         });
         if(!response.ok) throw new Error('404 or 500');
         return await response.text();
